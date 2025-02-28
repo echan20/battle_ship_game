d8699ce7f2a7d6794fb044f70355bd0ab7d8e9da
echan20 <echan20@oakgrove.school>
2025-02-28 09:27:28 +0000

    feat: add game grid

---
+++ grid.py
@@ -0,0 +1,42 @@
+def add_line_to_grid(grid: str, line: str):
+    if grid == "":
+        grid = line
+    else:
+        grid += "\n"
+        grid += line
+    return grid
+
+def generate_grid(x_size: int, y_size: int, values):
+    grid = ""
+    
+    for y in range(y_size):
+        line = ""
+        line2 = ""
+        for x in range(x_size):
+            value = ""
+            if line != "":
+                line += "│"
+                line2 += "│"
+
+            line2 += "───"
+
+            try:
+                value = values[y][x]
+            except:
+                value = "   "
+
+            if len(value) == 1:
+                value = " " + value + " "
+            elif len(value) == 2:
+                value = value + " "
+            
+            line += value
+
+        grid = add_line_to_grid(grid, line)
+        if (y + 1) < y_size:
+            grid = add_line_to_grid(grid, line2)
+
+    return grid
+
+grid = generate_grid(8, 8, [["M"]])
+print(grid)
